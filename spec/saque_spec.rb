c16dd2e7d56b8ff41f825bb396d710554dd3d0b3
Laylla  de Oliveira Rodrigues <laylla.rodrigues@C0167.local>
2021-08-06 13:30:30 -0300

    ajuste de estrutura

--- spec/saque_spec.rb
+++ spec/saque_spec.rb
@@ -1,24 +1,4 @@
-class ContaCorrente
-    attr_accessor :saldo, :mensagem
-
-    def initialize(saldo)
-        self.saldo = saldo
-    end
-
-    def saca(valor)
-        if (valor > self.saldo)
-            self.mensagem = 'Saldo insuficiente para saque :('
-        elsif (valor> 700.00)
-            self.mensagem = 'Limite máximo por saque é de R$700'
-            
-        else
-            self.saldo -= valor
-        end
-
-
-    end
-
-end
+require_relative '../app/bank'
 
 describe ContaCorrente do
 
